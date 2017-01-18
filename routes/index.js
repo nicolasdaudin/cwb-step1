51c4ad8691c42ed85c8369ec14e47e5d1ef2962c
Nicolas Daudin <nicolas.daudin@gmail.com>
2017-01-18 18:02:24 +0100

    test auto deploy heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CWB Step 1' });
 });
 
 /* GET Hello World page t. */
